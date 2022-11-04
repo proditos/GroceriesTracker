eb8af3ec338472ada8444e41c19434e5c4b474a5
Vladislav Konovalov <konovalov.phys@gmail.com>
2022-11-04 17:18:25 +0300

    update main

--- src/main/java/common/Main.java
+++ src/main/java/common/Main.java
@@ -1,15 +1,10 @@
 package common;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import dao.OrderDao;
-import dao.OrderProductDao;
-import dao.ProductDao;
-import entity.Order;
 import org.mariadb.jdbc.MariaDbDataSource;
-import parser.OrderParser;
+import parser.JsonParser;
 import parser.Parser;
-import service.OrderService;
+import dto.ReceiptDto;
+import service.ReceiptService;
 import java.nio.file.Paths;
 import java.sql.SQLException;
 
@@ -17,16 +12,14 @@ import java.sql.SQLException;
  * @author Vladislav Konovalov
  */
 public class Main {
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private static final String JSON_ROOT = "C:/Users/Prodi/Downloads/";
-    private static final String JSON_FILENAME = "ReceiptJson.json";
+    private static final String ROOT_FOLDER = "C:/Users/Prodi/Downloads/";
+    private static final String FILENAME = "file.json";
     private static final MariaDbDataSource DATA_SOURCE = new MariaDbDataSource();
-    private static final String DB_URL = "jdbc:mariadb://localhost:3306/lenta";
+    private static final String DB_URL = "jdbc:mariadb://localhost:3306/groceries";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "147896";
 
     static {
-        OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         try {
             DATA_SOURCE.setUrl(DB_URL);
             DATA_SOURCE.setUser(DB_USERNAME);
@@ -37,13 +30,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Parser<Order> parser = new OrderParser(OBJECT_MAPPER);
-        Order order = parser.parseJson(Paths.get(JSON_ROOT + JSON_FILENAME)).orElse(null);
+        Parser parser = new JsonParser();
+        ReceiptDto receiptDto = parser.parse(Paths.get(ROOT_FOLDER + FILENAME));
 
-        OrderDao orderDao = new OrderDao(DATA_SOURCE);
-        OrderProductDao orderProductDao = new OrderProductDao(DATA_SOURCE);
-        ProductDao productDao = new ProductDao(DATA_SOURCE);
-        OrderService orderService = new OrderService(orderDao, orderProductDao, productDao);
-        orderService.saveUnique(order);
+        ReceiptService receiptService = new ReceiptService(DATA_SOURCE);
+        receiptService.add(receiptDto);
     }
 }
